2aa024ed4569512b074359a1b06350f034144194
NataliyaPrys <nataliyapryss@gmail.com>
2020-04-21 11:43:26 +0300

    module-1 tasks-3,4,5

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,8 +1,9 @@
-/**
- * ucFirst
- * @param {string} str
- * @returns {string}
- */
+
 function ucFirst(str) {
-  // ваш код...
+  if ( str == ''){
+    return str;
+ } else {
+   return str[0].toUpperCase() + str.slice(1);
+  }
+    
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,9 @@
-/**
- * checkSpam
- * @param {string} str base
- * @returns {boolean}
- */
-function checkSpam(str) {
-  // ваш код...
+
+function checkSpam(str){
+  str = str.toLowerCase();
+  if(str.includes('1xBet')  || str.includes('1xbet') ||str.includes('XXX') || str.includes('xxx')){
+    return true;
+  } else {
+    return false;
+  }
 }
