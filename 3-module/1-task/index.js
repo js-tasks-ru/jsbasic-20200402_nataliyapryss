0b7aaad3107c17d9d8d43a2cee9e58e9fa7ea6b2
NataliyaPrys <nataliyapryss@gmail.com>
2020-05-04 01:27:01 +0300

    массив имен

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,7 +1,6 @@
-/**
- * @param   {{ name: string, age: number }[]} users
- * @returns {string[]}  объект
- */
 function namify(users) {
-  // ваш код...
+  
+  return users.map((user) => user.name);
+ 
+  
 }
