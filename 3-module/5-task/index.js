41720646bb1d1a22480b033217eeda89e42e97d6
NataliyaPrys <nataliyapryss@gmail.com>
2020-05-18 02:10:38 +0300

    Функция showSalary(data,age) и Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,8 +1,11 @@
-/**
- * Найти min/max в произвольной строке
- * @param   {string} str -  входные данные
- * @returns {{min:number, max:number}}  объект
- */
 function getMinMax(str) {
-  // ваш код...
+  let splitSeparation= str.split(/[\s,]+/);
+  let arrNum = splitSeparation.map((string) => parseFloat(string));
+  let filterArr = arrNum.filter((num) => isFinite(num));
+  let max = Math.max(...filterArr);
+  let min = Math.min(...filterArr);
+  let minMaxObject = {};
+  minMaxObject.max = max;
+  minMaxObject.min = min;
+  return minMaxObject;
 }
