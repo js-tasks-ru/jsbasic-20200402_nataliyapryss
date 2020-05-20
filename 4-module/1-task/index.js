ccd7d25b1cd62a1da4357e44f90afd7a08106d41
NataliyaPrys <nataliyapryss@gmail.com>
2020-05-20 23:00:43 +0300

    task makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,8 +1,15 @@
-/**
- * Генерация HTML списка друзей
- * @param {Object[]} friends
- * @return {HTMLUListElement}
- */
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let listLi = friends
+         .map ((item) => item.firstName + ' ' + item.lastName)
+         .map ((item) => `<li>${item}</li>`)
+         .join(' ');
+ 
+     let listUl = `<ul>${listLi}</ul>`;
+
+     let ul = document.createElement('ul');
+
+      //ul.insertAdjacentHTML('afterbegin', listLi );
+      ul.innerHTML = listLi;
+
+      return ul;
+};
